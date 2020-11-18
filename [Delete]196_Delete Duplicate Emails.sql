f36b2a13e7835720ccf8957e47d6cdf05a168f55
박지민 <jmpark7559@gmail.com>
2020-11-18 10:01:56 +0900

    중복값중 ID가 최소인 row 제거

--- [Delete]196_Delete Duplicate Emails.sql
+++ [Delete]196_Delete Duplicate Emails.sql
@@ -1,15 +1,22 @@
-/*
-# [LeetCode] 183. Customers Who Never Order
-# https://leetcode.com/problems/customers-who-never-order/
-# find all customers who never order anything.
-*/
-/* 내가 짠 SQL: 느림 1098 ms */
-SELECT Name AS Customers
-FROM Customers C LEFT JOIN Orders O ON C.Id = O.CustomerId
-WHERE O.CustomerId IS NULL
-;
+# [LeetCode] 196. Delete Duplicate Emails
+# https://leetcode.com/problems/delete-duplicate-emails/
+#  delete all duplicate email entries, keeping only unique emails based on its smallest Id.
+  
+# 내 소스 : 1505 ms
+# 
+# 에러발생 : "You can't specify target table 테이블명 for update in FROM clause"
+# 참고) https://jmkjb.tistory.com/entry/MySQLUpdateError
 
-/* Solution => 668 ms */
-SELECT Name AS Customers
-FROM Customers
-WHERE Id NOT IN (SELECT DISTINCT CustomerId FROM Orders);
+DELETE FROM Person 
+WHERE Id NOT IN (
+    SELECT ID 
+    FROM (SELECT MIN(Id) ID
+            FROM Person
+          GROUP BY Email) AS TMP)
+          
+          
+# Solution: 	1720 ms
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id
